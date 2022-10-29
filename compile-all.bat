0adfeee2f7f6e5c39477186d4889f551d99f406c
IsaacShelton <isaac.a.shelton@gmail.com>
2022-10-28 23:20:46 -0500

    Added helper compile-all.bat script

---
+++ compile-all.bat
@@ -0,0 +1,19 @@
+@echo off
+
+echo ^[WARNING^] Assuming `adept` command supports version `2.7`
+call :compile button
+call :compile edittext
+call :compile edittext_use
+call :compile opendialog
+call :compile text
+call :compile text_change
+call :compile window
+pause
+exit /b 0
+
+:compile 
+echo Compiling example '%~1'
+pushd %~1
+adept
+popd
+exit /b 0
