22c132177ef909a0ab62a1ada00c118956bfd467
IsaacShelton <isaac.a.shelton@gmail.com>
2022-10-29 20:27:27 -0500

    Added opendialog_use example and made some minor improvements to other examples

--- compile-all.bat
+++ compile-all.bat
@@ -5,6 +5,7 @@ call :compile button
 call :compile edittext
 call :compile edittext_use
 call :compile opendialog
+call :compile opendialog_use
 call :compile text
 call :compile text_change
 call :compile window
