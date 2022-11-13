c820b249d052b6037dc58210a5d1a4b086ef8226
IsaacShelton <isaac.a.shelton@gmail.com>
2022-11-13 11:53:24 -0600

    Updated compile-all.bat

--- compile-all.bat
+++ compile-all.bat
@@ -1,31 +1,31 @@
 
 @echo off
 
-echo ^[WARNING^] Assuming `adept` command supports version `2.8`
-call :compile modern/all_use
-call :compile modern/button
-call :compile modern/edittext
-call :compile modern/edittext_use
-call :compile modern/opendialog
-call :compile modern/opendialog_use
-call :compile modern/text
-call :compile modern/text_change
-call :compile modern/window
-call :compile old_style/all_use
-call :compile old_style/button
-call :compile old_style/edittext
-call :compile old_style/edittext_use
-call :compile old_style/opendialog
-call :compile old_style/opendialog_use
-call :compile old_style/text
-call :compile old_style/text_change
-call :compile old_style/window
+echo ^[WARNING^] Assuming `adept-nightly` exists and supports version `2.8`
+call :compile modern\all_use
+call :compile modern\button
+call :compile modern\edittext
+call :compile modern\edittext_use
+call :compile modern\opendialog
+call :compile modern\opendialog_use
+call :compile modern\text
+call :compile modern\text_change
+call :compile modern\window
+call :compile old_style\all_use
+call :compile old_style\button
+call :compile old_style\edittext
+call :compile old_style\edittext_use
+call :compile old_style\opendialog
+call :compile old_style\opendialog_use
+call :compile old_style\text
+call :compile old_style\text_change
+call :compile old_style\window
 pause
 exit /b 0
 
 :compile 
 echo Compiling example '%~1'
 pushd %~1
-adept
+call adept-nightly.bat
 popd
 exit /b 0
